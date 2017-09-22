5506d487fc684ed2f53dbb0210e09c8dd6b817f7
nycuma <31799327+nycuma@users.noreply.github.com>
2017-09-22 20:18:24 +0200

    view for 'Export' page

--- app/views/exportView.js
+++ app/views/exportView.js
@@ -1,3 +1,34 @@
 /**
  * Created by Julian Richter on 22 Sep 2017
  */
+
+'use strict';
+
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+Backbone.$ = $;
+
+var ExportView = Backbone.View.extend({
+    el: '#content',
+    template:  _.template($('#export-template').html()),
+
+    initialize: function(){
+        this.render();
+    },
+
+    events: {
+        'click .submitBtn': 'submit'
+    },
+
+    render: function () {
+        this.$el.html(this.template);
+        return this;
+    },
+
+    submit: function (evt) {
+        evt.preventDefault();
+    }
+});
+
+module.exports = ExportView;
