c98cf06b2003e85d24a2445c0926ea53db0defac
nycuma <31799327+nycuma@users.noreply.github.com>
2017-09-22 20:18:10 +0200

    view for 'About' page

--- app/views/aboutView.js
+++ app/views/aboutView.js
@@ -1,3 +1,26 @@
 /**
  * Created by Julian Richter on 22 Sep 2017
  */
+
+'use strict';
+
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+Backbone.$ = $;
+
+var AboutView = Backbone.View.extend({
+    el: '#content',
+    template:  _.template($('#about-template').html()),
+
+    initialize: function(){
+        this.render();
+    },
+
+    render: function () {
+        this.$el.html(this.template);
+        return this;
+    }
+});
+
+module.exports = AboutView;
