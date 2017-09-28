a89dbda2393a3f2be7a2da76d5645f56bc389707
nycuma <31799327+nycuma@users.noreply.github.com>
2017-09-28 18:55:49 +0200

    updated URL

--- app/collections/instructionsList.js
+++ app/collections/instructionsList.js
@@ -9,7 +9,7 @@ var Instruction = require('../models/singleInstruction');
 
 var InstructionsList = Backbone.Collection.extend({
     model: Instruction,
-    url: '/hypermedia-factory-2/app/modelData/instructionsData.json',
+    url: '/hypermedia-factory/app/modelData/instructionsData.json',
     parse: function (data) {
         return data.items;
     }
