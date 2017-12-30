ab1abbe5720aac9d44d2c9bb43d14fcdd487c9fb
nycuma <31799327+nycuma@users.noreply.github.com>
2017-12-30 23:25:43 +0100

    new function to set label on initialisation

--- app/models/term.js
+++ app/models/term.js
@@ -7,39 +7,29 @@
 
 var Backbone  = require('backbone');
 
-//TODO create submodel extending supermodel
+// TODO all links in descriptions should open in new tab/window
 var Term = Backbone.Model.extend({
     defaults: {
         value: '',
         prefix: '',
         label: '',
         descr: '',
-        superClasses: [],
+        //superClasses: [],
         isRdfClass: false,
         isRdfProperty: false,
         isAction: false
+    },
+
+    initialize: function () {
+        this.setLabel();
+    },
+
+    setLabel: function () {
+        this.set('label', this.get('prefix') + ': ' + this.get('value'));
     }
 });
 
-     /*,
-var RdfClass = Term.extend({
-    prefix: '',
-    superClasses: []
 
-});
-
-var RdfProperty = Term.extend({
-
-});
-
-var Action = Term.extend({
-
-});
-
-var NonRdfTerm = Term.extend({
-
-});
-*/
 
 
 module.exports = Term;
