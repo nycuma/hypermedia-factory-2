e75d792bc9ddf1b7f65af57715fac8e42b3abaf0
nycuma <31799327+nycuma@users.noreply.github.com>
2017-12-30 23:25:04 +0100

    added new RDF store for all RDF vocabs

--- app/collections/suggestionSource.js
+++ app/collections/suggestionSource.js
@@ -8,6 +8,7 @@ var Backbone = require('backbone');
 var _ = require('underscore');
 var $ = require('jquery');
 Backbone.$ = $;
+var N3 = require('n3');
 var Term = require('../models/term');
 
 // Import vocabularies to load
@@ -18,20 +19,23 @@ var SuggestionSource = Backbone.Collection.extend({
 
     model: Term,
 
-    // TODO Superclass RDFSource which defines basic parse methods
+    // RDF store contains terms from all RDF vocabularies
+    rdfStore: N3.Store(),
+
     initialize: function () {
         var self = this;
+
         var schemaOrgSource = new SchemaOrgSource();
         $.when(schemaOrgSource.fetch({parse: true})).then(function() {
-            self.add(schemaOrgSource.models);
-            //schemaOrgSource.reset();
+            self.rdfStore.addTriples(schemaOrgSource.store.getTriples());
+            self.rdfStore.addPrefix('schema', 'http://schema.org/');
         });
 
-
+        // non-RDF vocabulary is saved in local model
         var ianaLinkRelsSource = new IanaLinkRelsSource();
         $.when(ianaLinkRelsSource.fetch({parse: true})).then(function() {
             self.add(ianaLinkRelsSource.models);
-            //ianaLinkRelsSource.reset()
+            ianaLinkRelsSource.reset();
         });
 
     },
@@ -39,26 +43,49 @@ var SuggestionSource = Backbone.Collection.extend({
 
 
     getRDFClasses: function() {
-        // term = instance of BB.Model
         /*
         var result = this.filter(function (term) {
-            return term.get('isAction');
+            return term.get('isRdfClass');
         });
+        var result = this.where({isRdfClass: true, isAction: false});
+        return result;
         */
 
-        return this.where({isAction: true});
+
+        return this.reduce(function (filteredColl, term) {
+            if (term.get('isRdfClass') && !term.get('isAction')) {
+                filteredColl.push(term.toJSON());
+            }
+            return filteredColl;
+        }, []);
+
     },
 
     getRDFProps: function() {
-        return this.where({isRdfProperty: true});
+        return this.reduce(function (filteredColl, term) {
+            if (term.get('isRdfProperty')) {
+                filteredColl.push(term.toJSON());
+            }
+            return filteredColl;
+        }, []);
     },
 
     getActions: function () {
-        return this.where({isAction: true});
+        return this.reduce(function (filteredColl, term) {
+            if (term.get('isAction')) {
+                filteredColl.push(term.toJSON());
+            }
+            return filteredColl;
+        }, []);
     },
 
     getAllTermsFromVocab: function (prefix) {
-        return this.where({prefix: prefix});
+        return this.reduce(function (filteredColl, term) {
+            if (term.get('prefix' === prefix)) {
+                filteredColl.push(term.toJSON());
+            }
+            return filteredColl;
+        }, []);
     }
 });
 
