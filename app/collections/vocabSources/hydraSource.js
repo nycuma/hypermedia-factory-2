bbfc39f8d42c2f90d91972f4b4863876d5068c74
nycuma <31799327+nycuma@users.noreply.github.com>
2018-02-04 10:36:29 +0100

    removed file for Hydra vocab

--- app/collections/vocabSources/hydraSource.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Julian Richter on 21 Dec 2017
- */
--- app/collections/vocabSources/rdfSource.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Julian Richter on 25 Dec 2017
- */
-
-// TODO write common parse method that other sources can use
