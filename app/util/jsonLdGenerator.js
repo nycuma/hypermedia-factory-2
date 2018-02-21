542558b8e31e97a7f57ffb2dcf304e8b70988d15
nycuma <31799327+nycuma@users.noreply.github.com>
2018-02-21 23:48:57 +0100

    added code to generate Hydra ApiDocu

--- app/util/jsonLdGenerator.js
+++ app/util/jsonLdGenerator.js
@@ -25,34 +25,187 @@ var downloadJsonLdContexts = function(graph) {
 var downloadHydraAPI = function(graph, namespace) {
     console.log('downloadHydraAPI called');
 
+    var hydraApiObj = {};
+
     // get context info
     var contextObj = getApiDocContext(namespace);
+
+    hydraApiObj['@context'] = contextObj;
+    hydraApiObj['@id'] = '/docs.jsonld';
+    hydraApiObj['@type'] = 'hydra:ApiDocumentation';
+
+    console.log('hydraApiObj: ' + JSON.stringify(hydraApiObj, null, 2));
 
     var cells = graph.get('cells');
     contextObj['hydra:supportedClass'] = [];
     // each resource = one supported class
     cells.forEach(function(cell) {
         if (cell.get('type') === 'html.Node') {
-
             // for each class: get suppoted propteries & supported operations
-            var supportedClass = getSupportedClass(cell);
+            var outgoingLinks = graph.getConnectedLinks(cell, { outbound: true });
+            var incomingLinks = graph.getConnectedLinks(cell, { inbound: true });
+            var supportedClass = getSupportedClass(cell, outgoingLinks, incomingLinks);
             contextObj['hydra:supportedClass'].push(supportedClass);
         }
     });
+
 };
 
 function getApiDocContext(namespace) {
+    var contextObj = {};
+
+    contextObj['vocab'] = namespace;
+    contextObj['hydra'] = 'http://www.w3.org/ns/hydra/core#';
+    contextObj['rdf'] = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
+    contextObj['rdfs'] = 'http://www.w3.org/2000/01/rdf-schema#';
+    //contextObj['xmls'] = 'http://www.w3.org/2001/XMLSchema#';
+    contextObj['owl'] = 'http://www.w3.org/2002/07/owl#';
+    contextObj['domain'] = { "@id": "rdfs:domain", "@type": "@id" };
+    contextObj['range'] = { '@id': 'rdfs:range', '@type': '@id' };
+    //contextObj['subClassOf'] = { '@id': 'rdfs:subClassOf', '@type': '@id' };
+    contextObj['expects'] = { '@id': 'hydra:expects', '@type': '@id' };
+    contextObj['returns'] = { '@id': 'hydra:returns', '@type': '@id' };
+
+    return contextObj;
+}
+
+function getSupportedClass(cell, outboundLinks, incomingLinks) {
+    var resourceName = cell.prop('resourceName');
+
+    var classObj = {};
+    classObj['@id'] = resourceName.isCustom ? 'vocab:'+resourceName.value : resourceName.iri;
+    classObj['@type'] = 'hydra:Class'; // TODO Hydra Class or Link?
+    if(resourceName.isCustom) classObj['rdfs:comment'] = resourceName.customDescr;
+    classObj['rdfs:label'] = cell.prop('value'); // TODO get value with spaces?
+    classObj['hydra:title'] = cell.prop('value');
+    //classObj['hydra:description'] = cell.prop('isCustom') ? cell.prop('customDescr') : // TODO get RDF comment
+    classObj['hydra:supportedProperty'] = getSupportedProperties(cell, outboundLinks);
+
+    // only for links pointing back to the same resource
+    var returningLinks = getReturningLinks(cell, outboundLinks, incomingLinks);
+    classObj['hydra:supportedOperation'] = getSupportedOperationsForClass(cell, returningLinks);
+
+
+    return classObj;
 
 }
 
-function getSupportedClass(cell) {
+function getSupportedProperties(cell, outboundLinks) {
+    var supportedPropsArr = [];
+    var resourceAttrs = cell.prop('resourceAttrs');
+    var resourceName = cell.prop('resourceName');
 
+    // resource attributes from resource have literal values and are shipped as resource content
+    resourceAttrs.forEach(function(attr) {
+        var supportedProp = {}, hydraProp = {};
+        supportedProp['@type'] = 'hydra:SupportedProperty';
+
+        hydraProp['@id'] = attr.isCustom ? 'vocab:'+attr.value : attr.iri;
+        hydraProp['@type'] = 'rdf:Property';
+        hydraProp['rdfs:label'] = attr.value;
+        if(attr.isCustom) hydraProp['rdfs:comment'] = attr.customDescr;
+        hydraProp['domain'] = resourceName.isCustom ? 'vocab:'+resourceName.value : resourceName.iri;
+        //hydraProp['range'] = // TODO range of a property = data type
+
+        supportedProp['hydra:property'] = hydraProp;
+        supportedProp['hydra:title'] = attr.value;
+        //supportedProp['hydra:description'] = attr.isCustom ? attr.customDescr : //TODO get RDF comment
+        //supportedProp['hydra:required'] =  // TODO
+        //supportedProp['hydra:readonly'] =  // TODO
+        //supportedProp['hydra:writable'] =  // TODO
+
+        supportedPropsArr.push(supportedProp);
+    });
+
+
+    // get link relations of outgoing links
+    // (They have type hydra:Link and indicate to client that value is a dereferencable URL)
+    outboundLinks.forEach(function(link) {
+
+        var linkOperations = link.prop('operations');
+        if(linkOperations && linkOperations.length > 0) {
+
+            linkOperations.forEach(function(op) {
+                var supportedPropLink = {}, hydraPropLink = {};
+
+                hydraPropLink['@id'] = op.isCustom ? 'vocab:'+op.value : op.iri;
+                hydraPropLink['@type'] = 'hydra:Link';
+                hydraPropLink['rdfs:label'] = op.value;
+                if(op.isCustom) hydraPropLink['rdfs:comment'] = op.customDescr;
+                hydraPropLink['domain'] = resourceName.isCustom ? 'vocab:'+resourceName.value : resourceName.iri;
+                //hydraPropLink['range'] = // TODO range = resource name of target node
+
+
+                hydraPropLink['hydra:supportedOperation'] = getSupportedOperationsForProperty(link, op);
+
+                supportedPropLink['hydra:property'] = hydraPropLink;
+                supportedPropLink['hydra:title'] = op.value;
+                if(op.customDescr) supportedPropLink['hydra:description'] = op.customDescr;  //TODO get RDF comment
+                supportedPropLink['hydra:required'] =  'null';
+                //supportedProp['hydra:readonly'] =  // TODO
+                //supportedProp['hydra:writable'] =  // TODO
+
+                supportedPropsArr.push(supportedPropLink);
+            });
+        }
+
+
+    });
+
+    return supportedPropsArr;
+}
+
+function getSupportedOperationsForProperty(link, operation) {
+    var operationsForPropObj = {};
+    // TODO id?
+
+    return operationsForPropObj;
+
+    /**
+     *{
+                                "@id": "_:user_retrieve",
+                                "@type": "hydra:Operation",
+                                "method": "GET",
+                                "label": "Retrieves a User entity",
+                                "description": null,
+                                "expects": null,
+                                "returns": "vocab:User",
+                                "statusCodes": []
+                            }
+     */
+}
+
+function getSupportedOperationsForClass(cell, returningLinks) {
+    var operationsForClassArr = [];
+
+    return operationsForClassArr;
 }
 
 /**
- *
- * @param graph
+ * Returns all links for <cell> whose source node and target node are the same
+ * @param cell
+ * @param outboundLinks of cell
+ * @param incomingLinks of cell
+ * @return {Array}
  */
+function getReturningLinks(cell, outboundLinks, incomingLinks) {
+    var returningLinks = [];
+    if(outboundLinks && incomingLinks && outboundLinks.length > 0 && incomingLinks.length > 0) {
+        outboundLinks.forEach(function(outLink) {
+           var sourceId = outLink.get('source').id;
+           incomingLinks.forEach(function(inLink) {
+              var targetId = inLink.get('target').id;
+              if(sourceId === targetId === cell.id) {
+                  returningLinks.push(inLink);
+              }
+           });
+        });
+    }
+    console.log('found returning links for node ' + cell.id + ': ' + JSON.stringify(returningLinks));
+    return returningLinks;
+}
+
+
 function getProfileDocs(graph){
 
     var cells = graph.get('cells');
