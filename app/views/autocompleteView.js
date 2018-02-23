055df21d81de5554186f920c881d7e601b08f36a
nycuma <31799327+nycuma@users.noreply.github.com>
2018-02-23 23:00:45 +0100

    adjust code for new layout, removed unused code

--- app/views/autocompleteView.js
+++ app/views/autocompleteView.js
@@ -10,6 +10,8 @@ var $ = require('jquery');
 Backbone.$ = $;
 var sugSource;
 sugSource = require('../collections/suggestionSource');
+var methodsSugs;
+methodsSugs = require('../collections/methodSuggestions');
 var autocomplete = require('jquery-ui/ui/widgets/autocomplete');
 
 var AutocompleteView = Backbone.View.extend({
@@ -39,13 +41,13 @@ var AutocompleteView = Backbone.View.extend({
 
             this.createInputField(this.options.resourceNameValue, this.options.resourceNamePrefix);
 
-        } else if(this.id === 'relation') {
+        } else if(this.id.match(/relation/)) {
 
             console.log('new suggestionInputView relation: value and prefix: '
                 + this.options.resourceNameValue + ', ' + this.options.resourceNamePrefix);
 
             this.createInputField(this.options.resourceNameValue, this.options.resourceNamePrefix);
-        } else if(this.id.match(/operation/)) {
+        } else if(this.id.match(/action/)) {
             this.createInputField();
         }
 
@@ -188,8 +190,8 @@ var AutocompleteView = Backbone.View.extend({
 
                 if(self.id === 'resourceName') results = self.getSourceResName(request.term, value, prefix);
                 else if (self.id.match(/resourceAttr/)) results = self.getSourceResAttribute(request.term, value, prefix);
-                else if(self.id === 'relation') results = self.getSourceLinkRelation(request.term, value, prefix);
-                else if(self.id.match(/operation/)) results = self.getSourceOperation(request.term);
+                else if(self.id.match(/relation/)) results = self.getSourceLinkRelation(request.term, value, prefix);
+                else if(self.id.match(/action/)) results = self.getSourceOperation(request.term);
 
                 response(results);
             },
@@ -201,17 +203,20 @@ var AutocompleteView = Backbone.View.extend({
             select: function(event, ui) {
                 var prefix = sugSource.getPrefixFromLabel(ui.item.label);
 
+                // update method suggestions depending on selected relation and action
+                if(self.id.match(/action/) || self.id.match(/relation/)) self.updateMethodSuggestions(event.target.id, ui.item.value, prefix);
+
                 // refresh input fields for resources attributes (suggest only properties for selected resource name)
-                if(this.id === 'resourceName') self.trigger('resourceNameSelected', {value: ui.item.value, prefix: prefix});
+                if(self.id === 'resourceName') self.trigger('resourceNameSelected', {value: ui.item.value, prefix: prefix});
 
-                // uncheck 'custom term'-checkbox and hide customTermDescription
-                self.removeInputCustomTermDescription(this);
-
-                // display complete IRI next to autocomplete field
+                // display complete IRI below autocomplete field
                 self.fillInputFieldIri(ui.item.label, ui.item.value);
 
-                // save prefix in hidden input field
-                self.writePrefixToHiddenInputField(prefix);
+                // display complete description next to autocomplete field
+                self.fillInputFieldTermDescr(ui);
+
+                // save prefix as atribute of input field
+                self.updateCurrentPrefix(prefix);
 
             },
 
@@ -222,118 +227,30 @@ var AutocompleteView = Backbone.View.extend({
         }).element.attr({ type: 'text', id: this.id})
             .appendTo('#'+this.id+'InputField');
 
-        this.createHiddenFieldForPrefix('#'+this.id+'InputField');
-        this.registerAutocompleteInputChangeEvent();
-
+        if(!this.id.match(/action/)) {
+            this.registerInputChangeEvent();
+        }
     },
-
-    createInputFielResAttr: function(value, prefix) {
-
-        console.log('createInputFielResAttr: ' + value + ', ' + prefix);
-
-        var self = this;
-        new autocomplete({
-            minLength: 2,
-
-            source: function(request, response) {
-
-                var results = [];
-                var regEx = new RegExp(request.term, 'i');
-
-
-                if(value && prefix) { // get RDF properties only for entered resource name and its super types
-
-                    var prefixIRI = sugSource.getPrefixIRIFromPrefix(prefix);
-
-                    var typesAsIri = self.getRDFSuperClasses(prefixIRI+value);
-                    //console.log('TYPES AS IRI: ' + JSON.stringify(typesAsIri));
-
-                    typesAsIri.forEach(function (typeIri) {
-
-                        sugSource.rdfStore.forSubjectsByIRI(function (subject) {
-                            var val = sugSource.getTermFromIRI(subject);
-                            if(val.match(regEx)) {
-                                results.push({
-                                    value: val,
-                                    label: prefix + ': ' + val
-                                });
-                            }
-                        }, 'http://schema.org/domainIncludes', typeIri);
-
-                    });
-
-                    response(results);
-
-
-                } else { // get all RDF properties that match entered characters
-
-                    sugSource.rdfStore.forSubjectsByIRI(function (subject) {
-                        var val = sugSource.getTermFromIRI(subject);
-
-                        if(val.match(regEx)) {
-                            results.push({
-                                value: val,
-                                label: sugSource.getLabelFromIRI(subject)
-                            });
-                        }
-                    }, null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property');
-
-                    response(results.slice(0, 15)); // max. 15 results
-                }
-            },
-
-            focus: function(event, ui) {
-                self.displayTermDescription(event, ui);
-            },
-
-            select: function(event, ui) {
-                // uncheck checkbox and hide customTermDescription
-                self.removeInputCustomTermDescription(this);
-
-                // display complete IRI next to autocomplete field
-                self.fillInputFieldIri(ui.item.label, ui.item.value);
-
-                // save prefix in hidden input field
-                var prefix = sugSource.getPrefixFromLabel(ui.item.label);
-                self.writePrefixToHiddenInputField(prefix);
-            },
-
-            close: function () {
-                $('#termDesc').hide().empty();
-            }
-
-        }).element.attr({ type: 'text', id: this.id})
-            .appendTo('#'+this.id+'InputField');
-
-        this.createHiddenFieldForPrefix('#'+this.id+'InputField');
-        this.registerAutocompleteInputChangeEvent();
-
-    },
-
 
     displayTermDescription: function(event, ui) {
         var position = {top: event.pageY + 15, left: event.pageX + 25};
+        var termDescr = this.getTermDescription(ui);
+        $('#termDesc').html(termDescr).css(position).show();
+    },
 
+    getTermDescription: function(ui) {
+        var descr = '';
         if(ui.item.descr) {
-            $('#termDesc').html(ui.item.descr).css(position).show();
+            descr = ui.item.descr;
         } else {
             var prefixIRI = sugSource.getPrefixIRIFromLabel(ui.item.label);
             var rdfComment = sugSource.rdfStore.getObjectsByIRI(prefixIRI+ui.item.value, 'http://www.w3.org/2000/01/rdf-schema#comment');
 
             if(rdfComment) { //TODO what if no RDF comment available?
-                $('#termDesc').html(rdfComment[0].substr(1, rdfComment[0].length-2)).css(position).show();
+                descr = rdfComment[0].substr(1, rdfComment[0].length-2);
             }
         }
-    },
-
-    createHiddenFieldForPrefix: function(appendToEl) {
-        $(appendToEl).append('<input type="hidden" id="'+this.id+'Prefix'+'" class="prefixInput" value="">');
-    },
-
-    writePrefixToHiddenInputField: function(prefix) {
-        //console.log('writePrefixToHiddenInputField called on: ' +  this.id);
-        //console.log('writePrefixToHiddenInputField prefix: ' +  prefix);
-        $('#'+this.id+'Prefix').val(prefix);
+        return descr;
     },
 
     // goes up the class hierarchy until 'ignore'
@@ -380,10 +297,30 @@ var AutocompleteView = Backbone.View.extend({
         //$(evt.target).parent().parent().next().toggle(); --> bug
     },
 
-    removeInputCustomTermDescription: function (inputElem) {
-        $('#'+this.id+'CheckCustomTerm').prop('checked', false);
-        $(inputElem).parent().parent().next().next().next().hide();
-        $('#'+this.id+'CustomTermDescr').val('');
+    updateMethodSuggestions: function(targetId, value, prefix) {
+        // get options suggestions
+        //.val('DELETE');
+        var $dropdown = $('#methodDropdown' + targetId.substring(targetId.length-1));
+
+        $dropdown.empty();
+
+        var methods = methodsSugs.getMethodSuggestion(value, prefix);
+
+        console.log('updateMethodSuggs: methods: ' + JSON.stringify(methods));
+
+        /*
+        $.when(methodsSugs.fetch({parse: true})).then(function() {
+            JSON.stringify('updateMethodSuggestions')
+        });
+        */
+
+        /*
+         $.each(newOptions, function(key,value) {
+         $el.append($("<option></option>")
+         .attr("value", value).text(key));
+         });
+         */
+
     },
 
     fillInputFieldIri: function(label, value) {
@@ -395,26 +332,46 @@ var AutocompleteView = Backbone.View.extend({
         }
     },
 
+    fillInputFieldTermDescr: function(ui) {
+        var descrHTML = this.getTermDescription(ui);
+        $('#'+this.id+'TermDescr').val(this.getStringFromHTML(descrHTML));
+    },
+
+    getStringFromHTML: function(html) {
+        var text = $('<p>'+html+'</p>').text();
+        text = text.replace(/\r?\n|\r|\t/g, ' '); // remove linebreaks and tabs
+        return text;
+    },
+
     setCustomIRI: function(targetId) {
-        if($('#'+this.id+'CheckCustomTerm').prop('checked')) {
-            $('#'+targetId+'Iri').val('{myURL}/vocab#' + $('#'+targetId).val());
-        }
+        $('#'+targetId+'Iri').val('{myURL}/vocab#' + $('#'+targetId).val());
     },
 
     resetCustomIRI: function() {
         $('#'+this.id+'Iri').val('');
     },
 
-    registerAutocompleteInputChangeEvent: function () {
-        // register change events for autocomplete input fields
-        //console.log('registered AutocompleteInputChangeEvent on: ' + this.id);
-        var self = this;
-        $('.ui-autocomplete-input').bind('change',function(evt) {
-            self.setCustomIRI(evt.target.id);
-        });
+    updateCurrentPrefix: function(prefix) {
+        $('#'+this.id).attr({'term-prefix': prefix, 'isCustom': false});
+    },
+
+    setAsCustom: function (targetId) {
+        $('#'+targetId).attr({'term-prefix': '', 'isCustom': true});
+    },
+
+    registerInputChangeEvent: function () {
+        $('#'+this.id).bind('change',_.bind(function(evt) {
+            this.setCustomIRI(evt.target.id);
+            this.setAsCustom(evt.target.id);
+        }, this));
+
+        $('#'+this.id+'TermDescr').bind('change',_.bind(function(evt) {
+            var targetId = evt.target.id;
+            var firstPart = targetId.substr(0, targetId.indexOf('TermDescr'));
+            this.setCustomIRI(firstPart);
+            this.setAsCustom(firstPart);
+        }, this));
     }
-
-
 });
 
 module.exports = AutocompleteView;
