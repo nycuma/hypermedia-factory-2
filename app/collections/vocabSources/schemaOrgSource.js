56f40b9ee1126219bbf1f6a083cf35c01adba87d
nycuma <31799327+nycuma@users.noreply.github.com>
2018-03-09 13:03:16 +0100

    placed some Schema.org specific terms with standard terms from RDF Schema; removed unused code

--- app/collections/vocabSources/schemaOrgSource.js
+++ app/collections/vocabSources/schemaOrgSource.js
@@ -18,6 +18,11 @@ var SchemaOrgSource = Backbone.Collection.extend({
     // DEV:
     url: '../app/static/vocabs/schema.ttl',
 
+    schemaDomain: 'http://schema.org/domainIncludes',
+    schemaRange: 'http://schema.org/rangeIncludes',
+    rdfsDomain: 'http://www.w3.org/2000/01/rdf-schema#domain',
+    rdfsRange: 'http://www.w3.org/2000/01/rdf-schema#range',
+
     store: N3.Store(null, {prefixes : {schema : 'http://schema.org'}}),
 
     sync: function (method, model, options) {
@@ -45,55 +50,39 @@ var SchemaOrgSource = Backbone.Collection.extend({
     parse: function (turtle) {
         console.log('Schema.org parser called');
 
-        var parser = N3.Parser({ format: 'Turtle' });
+        var parser = N3.Parser({format: 'Turtle'});
         var self = this;
+        var domainPred = 0, rangePred = 0;
+        parser.parse(turtle, function (error, triple) {
 
-        parser.parse(turtle, function(error, triple) {
             if (triple) {
-                self.store.addTriple(triple.subject, triple.predicate, triple.object);
+
+                // Schema.org defines a new term to describe the domain and range of properties,
+                // instead of using the standard rdfs:domain and rdfs:range.
+                // The Schema.org terms 'schema:domainIncludes' and 'schema:rangeIncludes' are replaced
+                // by the standard terms, because this makes it easier to search in multiple RDF vocabs.
+                if (triple.predicate == self.schemaDomain) {
+                    self.store.addTriple(triple.subject, self.rdfsDomain, triple.object);
+                    domainPred++;
+                }
+                else if (triple.predicate == self.schemaRange) {
+                    self.store.addTriple(triple.subject, self.rdfsRange, triple.object);
+                    rangePred++;
+                }
+                else {
+                    self.store.addTriple(triple.subject, triple.predicate, triple.object);
+                }
+
             } else {
                 console.log('finished parsing Schema.org file');
+                console.log('\treplaced ' + domainPred + ' domain predicates');
+                console.log('\treplaced ' + rangePred + ' range predicates');
             }
-            if(error) {
+            if (error) {
                 console.log('error while parsing Schema.org turtle file');
             }
 
         });
-
-
-        // synchron
-        /*
-        triples.forEach(_.bind(function (triple) {
-            this.store.addTriple(triple.subject, triple.predicate, triple.object);
-        }, this));
-        */
-
-        /*
-        // filter unique Schema.org properties
-        this.store.forSubjectsByIRI(_.bind(function (subject) {
-            var rdfComment = this.store.getObjectsByIRI(subject, 'http://www.w3.org/2000/01/rdf-schema#comment')[0];
-            self.add(new Term({ value: subject.substr(18),
-                                prefix: 'schema',
-                                isRdfProperty: true,
-                                descr : rdfComment.substr(1, rdfComment.length-2)
-            }));
-        }, this), null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property');
-
-
-        // filter all unique Schema.org classes
-        this.store.forSubjectsByIRI(_.bind(function (subject) {
-
-            var rdfComment = this.store.getObjectsByIRI(subject, 'http://www.w3.org/2000/01/rdf-schema#comment')[0];
-            self.add(new Term({ value: subject.substr(18),
-                                prefix: 'schema',
-                                isRdfClass: true,
-                                isAction: subject.substr(18).includes('Action'),
-                                descr : rdfComment.substr(1, rdfComment.length-2)
-            }));
-        }, this), null, 'http://www.w3.org/2000/01/rdf-schema#Class');
-        */
-
-        //return this.models;
     }
 });
 
